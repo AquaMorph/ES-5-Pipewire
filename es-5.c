1d0e1f84d359bed5d58d3fb428de0db210fcca2c
Christian Colglazier <christian@cacolglazier.com>
2021-10-09 15:08:00 -0400

    Converted to filter

--- es-5.c
+++ es-5.c
@@ -1,36 +1,110 @@
+#include <spa/param/latency-utils.h>
+
 #include <pipewire/pipewire.h>
+
+struct data;
+
+struct port {
+	struct data *data;
+};
 
 struct data {
   struct pw_main_loop *loop;
-  struct pw_stream *stream;
+  struct pw_filter *filter;
+  struct port *in_port_one;
+  struct port *in_port_two;
+  struct port *in_port_three;
+  struct port *in_port_four;
+  struct port *in_port_five;
+  struct port *in_port_six;
+  struct port *in_port_seven;
+  struct port *in_port_eight;
+  struct port *out_port;
 };
 
-static void on_process(void *userdata) {}
+static void on_process(void *userdata, struct spa_io_position *position) {}
 
-static const struct pw_stream_events stream_events = {
-  PW_VERSION_STREAM_EVENTS,
-  .process = on_process,
+static void do_quit(void *userdata, int signal_number) {
+  struct data *data = userdata;
+  pw_main_loop_quit(data->loop);
+}
+
+static const struct pw_filter_events filter_events = {
+	PW_VERSION_FILTER_EVENTS,
+	.process = on_process,
 };
+
+struct port* add_port(char* name, struct data data) {
+  return  pw_filter_add_port(data.filter,
+			     PW_DIRECTION_INPUT,
+			     PW_FILTER_PORT_FLAG_MAP_BUFFERS,
+			     sizeof(struct port),
+			     pw_properties_new(
+					       PW_KEY_FORMAT_DSP, "32 bit float mono audio",
+					       PW_KEY_PORT_NAME, name,
+					       NULL),
+			     NULL, 0);
+}
 
 int main(int argc, char *argv[]) {
   struct data data = { 0, };
+  const struct spa_pod *params[1];
+  uint8_t buffer[1024];
+  struct spa_pod_builder b = SPA_POD_BUILDER_INIT(buffer, sizeof(buffer));
+  
   pw_init(&argc, &argv);
 
   data.loop = pw_main_loop_new(NULL);
 
-  data.stream = pw_stream_new_simple(
+  pw_loop_add_signal(pw_main_loop_get_loop(data.loop), SIGINT, do_quit, &data);
+  pw_loop_add_signal(pw_main_loop_get_loop(data.loop), SIGTERM, do_quit, &data);
+
+  data.filter = pw_filter_new_simple(
 				     pw_main_loop_get_loop(data.loop),
-				     "audio-src",
+				     "ES-5",
 				     pw_properties_new(
 						       PW_KEY_MEDIA_TYPE, "Audio",
-						       PW_KEY_MEDIA_CATEGORY, "Duplex",
+						       PW_KEY_MEDIA_CATEGORY, "Filter",
 						       PW_KEY_MEDIA_ROLE, "DSP",
 						       NULL),
-				     &stream_events,
+				     &filter_events,
 				     &data);
 
+  data.in_port_one = add_port("input_1", data);
+  data.in_port_two = add_port("input_2", data);
+  data.in_port_three = add_port("input_3", data);
+  data.in_port_four = add_port("input_4", data);
+  data.in_port_five = add_port("input_5", data);
+  data.in_port_six = add_port("input_6", data);
+  data.in_port_seven = add_port("input_7", data);
+  data.in_port_eight = add_port("input_8", data);
 
-  pw_stream_destroy(data.stream);
+  data.out_port = pw_filter_add_port(data.filter,
+				     PW_DIRECTION_OUTPUT,
+				     PW_FILTER_PORT_FLAG_MAP_BUFFERS,
+				     sizeof(struct port),
+				     pw_properties_new(
+						       PW_KEY_FORMAT_DSP, "32 bit float mono audio",
+						       PW_KEY_PORT_NAME, "output",
+						       NULL),
+				     NULL, 0);
+
+  params[0] = spa_process_latency_build(&b,
+					SPA_PARAM_ProcessLatency,
+					&SPA_PROCESS_LATENCY_INFO_INIT(
+								       .ns = 10 * SPA_NSEC_PER_MSEC
+								       ));
+
+  if (pw_filter_connect(data.filter,
+			PW_FILTER_FLAG_RT_PROCESS,
+			params, 1) < 0) {
+    fprintf(stderr, "can't connect\n");
+    return -1;
+  }
+
+  pw_main_loop_run(data.loop);
+
+  pw_filter_destroy(data.filter);
   pw_main_loop_destroy(data.loop);
   return 0;
 }
